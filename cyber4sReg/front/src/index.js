4093c10f662abf91e2d9b9b3cdaad3f630456d7d
noamgolani <noam.golani@gmail.com>
2021-10-20 14:58:38 +0300

    from js almost done

--- cyber4sReg/front/src/index.js
+++ cyber4sReg/front/src/index.js
@@ -3,5 +3,30 @@ import './styles/style.css';
 
 import 'bootstrap';
 import $ from 'jquery';
+import axios from 'axios';
 
-$('body').append(' World');
+$('form').on('submit', (event) => {
+  event.preventDefault();
+});
+
+$('#sbmt').one('click', (event) => {
+  event.preventDefault();
+  const name = $('input#name').val();
+  const age = $('input#age').val();
+  const ability = [
+    ...$('input[type="checkbox"]').map(
+      (_, checkEl) => checkEl.checked && $(`[for="${checkEl.id}"]`).html(),
+    ),
+  ].filter((val) => val);
+
+  axios
+    .post('http://127.0.0.1:5000/api', {
+      name,
+      age,
+      ability,
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => console.log);
+});
